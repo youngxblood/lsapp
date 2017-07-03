0ae1073d88de7081224255cc4b7708379b7b7880
crazyeight68@gmail.com <Cararoo68>
2017-07-03 09:46:22 -0700

    Created 'Post' controller as well as created basic routing.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>{{config('app.name', 'LSApp')}}</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
 </head>
 <body>
-    @yield('content')
+@include('inc.navbar')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center">
 <h1>{{$title}}</h1>
 <p>This is the Laravel application that I will be working on.</p>
+<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg">Register</a>
+</div>
 @endsection
