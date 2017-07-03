36524824dcf1f3910a50fccc8b660631e39d5b0b
crazyeight68@gmail.com <Cararoo68>
2017-07-02 23:04:46 -0700

    Initial commit.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name', 'LSApp')}}</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
